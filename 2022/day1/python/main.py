a4d99267772596324b94b0fcd5d925a0cb63c313
Fattigman <jacob.karlstrom@gmail.com>
2022-12-20 20:48:58 +0100

    solution day 2

--- 2022/day1/python/main.py
+++ 2022/day1/python/main.py
@@ -1,9 +1,16 @@
 biggest = 0
 temp = 0
+biggest_list = []
 for line in  open('input.txt').readlines():
     if line.strip() == '':
+        if len(biggest_list) < 3 or temp > min(biggest_list):
+            if len(biggest_list) < 3:
+                biggest_list.append(temp)
+            else:
+                biggest_list[biggest_list.index(min(biggest_list))] = temp
         biggest = max(biggest, temp)
         temp = 0
     else:
         temp += int(line)
 print(biggest)
+print(sum(biggest_list))
